f3a36030eb396b5e4ae62e612784d3ad3d9a48a5
MegAMog <nastymarch.education@gmail.com>
2025-08-03 12:45:37 +0100

    Added step 2 - removed PII data (customer name and card number).

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ import utils.path as path
 csv_file = os.path.join(path.data_dir, "leeds_09-05-2023_09-00-00_done.csv")
 
 #CSV-files don't have headers. Create column_names list of headers.
-column_names = ['order_date', 'branch_name', 'customer', 'order_snapshot', 'bill', 'payment_type', 'card_number']
+column_names = ['order_date', 'branch_name', 'customer_name', 'order_snapshot', 'bill', 'payment_type', 'customer_card_number']
 
 
 #Step 1: extract data from csv file into pandas DataFrame object
@@ -23,3 +23,12 @@ print(raw_data.info())
 # #Retrieve header and first 10 rows and print int out -> use head() 
 # #Note: to see header and last 10 rows - use tail()
 print(raw_data.head(10))
+
+
+#Step 2: remove PII - customer name and card number
+#https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.drop.html
+pii_column_names = ['customer_name', 'customer_card_number']
+cleaned_data = raw_data.drop(columns = pii_column_names)
+print(cleaned_data)
+
+
